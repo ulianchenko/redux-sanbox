df8da5bca1900aa58d4d6bdc4ba18ac6d006dfe7
Ulianchenko <ulianchenko@gmail.com>
2021-11-03 17:30:47 +0200

    bindActions, connect and mapDispatchToProps functions were used

---
+++ src/actions.js
@@ -0,0 +1,12 @@
+export const inc = () => {
+  return {type: "INC"};
+};
+
+export const dec = () => ({type: "DEC"});
+
+export const rnd = () => {
+  return {
+    type: "RND",
+    payload: Math.floor(Math.random()*10)
+  };
+};
--- src/index.js
+++ src/index.js
@@ -1,52 +1,57 @@
 import { createStore } from "redux";
-
-const reducer = (state = 0, action) => {
-  switch (action.type) {
-    case "INC":
-      return state + 1;
-
-    case "DEC":
-      return state - 1;
-
-    case "RND":
-      return state + action.payload;
-
-    default:
-      return state;
-  }
-};
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import App from "./components/app"
+import Counter from "./components/counter";
+import reducer from "./reducer";
+// import * as actions from "./actions";
 
 const store = createStore(reducer);
+// const { dispatch } = store;
+
+// Example
+// const mul = (payload) => ({type: "MUL", payload: payload});
+// const mulDispatch = (payload) => store.dispatch(mul(payload));
+// Example
+
+// const bindActionCreator = (creator, dispatch) => (...args) => {
+//   dispatch(creator(...args));
+// }
+// const { inc, dec, rnd } =
+//   bindActionCreators(actions, dispatch);
+
+// const incDispatch = bindActionCreators(inc, dispatch);
+// const decDispatch = bindActionCreators(dec, dispatch);
+// const rndDispatch = bindActionCreators(rnd, dispatch);
+
 // store.subscribe(() => {
 //   console.log(store.getState());
 // });
 // store.dispatch({type: "INC"});
 // store.dispatch({type: "INC"});
 
-const inc = () => {
-  return {type: "INC"};
-};
+// document.getElementById("mul").addEventListener("click", () => {
+//   const payload = Math.floor(Math.random()*100);
+//   mulDispatch(payload);
+// });
 
-const dec = () => ({type: "DEC"});
+// document.getElementById("inc").addEventListener("click", inc);
 
-const rnd = (payload) => ({type: "RND", payload});
+// document.getElementById("dec").addEventListener("click", dec);
 
+// document.getElementById("rnd").addEventListener("click", () => {
+//   const payload = Math.floor(Math.random()*10);
+//   rnd(payload);
+// });
 
-document.getElementById("inc").addEventListener("click", () => {
-  store.dispatch(inc());
-});
+// const update = () => {
+  ReactDOM.render(
+    <Provider store={store}>
+      <App/>
+    </Provider>,
+    document.getElementById("root"));
+  // document.getElementById("counter").innerHTML = store.getState();
+// };
 
-document.getElementById("dec").addEventListener("click", () => {
-  store.dispatch(dec());
-});
-
-document.getElementById("rnd").addEventListener("click", () => {
-  const payload = Math.floor(Math.random()*10);
-  store.dispatch(rnd(payload));
-});
-
-const update = () => {
-  document.getElementById("counter").innerHTML = store.getState();
-};
-
-store.subscribe(update);
+// update();
+// store.subscribe(update);
